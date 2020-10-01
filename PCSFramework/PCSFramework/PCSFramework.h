3a646ab71998c89050ab4e1e2d6419f4400b4c89
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-09-30 22:24:28 -0500

    initial commit

---
+++ PCSFramework/PCSFramework/PCSFramework.h
@@ -0,0 +1,18 @@
+//
+//  PCSFramework.h
+//  PCSFramework
+//
+//  Created by Priyank Shah on 9/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PCSFramework.
+FOUNDATION_EXPORT double PCSFrameworkVersionNumber;
+
+//! Project version string for PCSFramework.
+FOUNDATION_EXPORT const unsigned char PCSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PCSFramework/PublicHeader.h>
+
+
---
+++ PCSFramework/PCSFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PCSFramework
+//
+//  Created by Priyank Shah on 9/30/20.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+        func dosomething() -> String {
+            return "Did something"
+        }
+    }
+}
